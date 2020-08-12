8dc2ed519ccecfb6321785c517bbeca2b022006b
anthony <nvhoanganh1909@gmail.com>
2020-08-13 09:19:59 +1000

    initial commit

---
+++ index.js
@@ -0,0 +1,17 @@
+const express = require('express')
+
+const rookout = require('rookout');
+rookout.start({
+  token: 'ecbae15c971c010f7d37ef6f93d8f55b3729118c32bb1d5f82ae3262a09145d8'
+});
+
+const app = express()
+const port = 3000
+
+app.get('/', (req, res) => {
+  res.send('Hello World!')
+})
+
+app.listen(port, () => {
+  console.log(`Example app listening at http://localhost:${port}`)
+})
